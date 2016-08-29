1aad362f2a75be801e1309369585bbdfeee884e4
Jonathan Rajotte <jraj@microsoft.com>
2016-08-29 19:21:16 -0400

    d

--- windows-powershell4/install.cmd
+++ windows-powershell4/install.cmd
@@ -3,7 +3,11 @@
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSVERSION=32BIT || set OSVERSION=64BIT
 
 if %OSVERSION%==32BIT (
-  Windows6.1-KB2506143-x86.msu /quiet
+  call Windows6.1-KB2506143-x86.msu /quiet /norestart
 ) else (
-  Windows6.1-KB2506143-x64.msu /quiet
+  call Windows6.1-KB2506143-x64.msu /quiet /norestart
+)
+
+if %ERRORLEVEL% == 0 (
+  shutdown.exe /r /t 00
 )
