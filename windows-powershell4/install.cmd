e554c9d1f02193e3a52ba7a5a44a38b5fffd2e94
Jonathan Rajotte <jraj@microsoft.com>
2016-08-29 15:46:21 -0400

    d

---
+++ windows-powershell4/install.cmd
@@ -0,0 +1,10 @@
+@echo OFF
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSVERSION=32BIT || set OSVERSION=64BIT
+
+if %OSVERSION%==32BIT (
+  Windows6.1-KB2506143-x86.msu /quiet
+)
+else (
+  Windows6.1-KB2506143-x64.msu /quiet
+)
